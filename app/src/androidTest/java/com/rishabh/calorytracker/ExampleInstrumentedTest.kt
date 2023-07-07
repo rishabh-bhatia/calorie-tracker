bac978856ec4b430232169cd237905ffca7dc159
Rishabh Bhatia <ojas14@gmail.com>
2023-07-07 23:22:55 +1000

    Refactor package name
    Renamed the package name from `plcoding` to `rishabh`

--- app/src/androidTest/java/com/rishabh/calorytracker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rishabh/calorytracker/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.plcoding.calorytracker", appContext.packageName)
+        assertEquals("com.rishabh.calorytracker", appContext.packageName)
     }
 }
