559e15eea10dad7fbcde0dfbff07017fe027f2a7
Rishabh Bhatia <ojas14@gmail.com>
2023-09-06 00:20:10 +1000

    Add Nutrient goal screen
    Added Nutrient Goal, related components and viewmodels.

--- app/src/main/java/com/rishabh/calorytracker/MainActivity.kt
+++ app/src/main/java/com/rishabh/calorytracker/MainActivity.kt
@@ -19,6 +19,7 @@ import com.rishabh.onboarding_presentation.age.AgeScreen
 import com.rishabh.onboarding_presentation.gender.GenderScreen
 import com.rishabh.onboarding_presentation.goal.GoalScreen
 import com.rishabh.onboarding_presentation.height.HeightScreen
+import com.rishabh.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.rishabh.onboarding_presentation.weight.WeightScreen
 import com.rishabh.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -65,7 +66,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(onNavigate = navController::navigate)
