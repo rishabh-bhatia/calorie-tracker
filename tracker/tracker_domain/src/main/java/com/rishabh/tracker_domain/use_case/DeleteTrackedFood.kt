5a88ccb233c09900dcc03f1e28021a356e7d4ecf
Rishabh Bhatia <ojas14@gmail.com>
2023-09-08 16:18:54 +1000

    Add Tracker usecases and DI

---
+++ tracker/tracker_domain/src/main/java/com/rishabh/tracker_domain/use_case/DeleteTrackedFood.kt
@@ -0,0 +1,13 @@
+package com.rishabh.tracker_domain.use_case
+
+import com.rishabh.tracker_domain.model.TrackedFood
+import com.rishabh.tracker_domain.repository.TrackerRepository
+
+class DeleteTrackedFood(
+    private val repository: TrackerRepository
+) {
+
+    suspend operator fun invoke(trackedFood: TrackedFood) {
+        repository.deleteTrackedFood(trackedFood)
+    }
+}
